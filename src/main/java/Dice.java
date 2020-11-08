a487578676baf1dd0a4bd148348d99e26e1dfec2
pompy <nicole.lynn.pompei@gmail.com>
2020-11-08 15:16:50 -0500

    dicey tossAndSum method and test completed

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,27 @@
+import java.util.Random;
+
 public class Dice {
+    private Integer numOfDice;
 
+    public Dice(Integer numOfDice){
+        this.numOfDice = numOfDice;
+    }
 
+    public Integer getNumOfDice() {
+        return numOfDice;
+    }
+
+    public Integer tossAndSum(){
+
+        Integer sumOfTosses = 0;
+
+        for(int i = 0; i < numOfDice; i++){
+
+            Random toss = new Random();
+            Integer resultsOfToss = toss.nextInt(6) + 1;
+            sumOfTosses += resultsOfToss;
+        }
+        return sumOfTosses;
+
+    }
 }
---
+++ src/test/java/DiceTest.java
@@ -0,0 +1,77 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Random;
+
+
+public class DiceTest {
+
+    @Test
+    public void diceConstructorTest() {
+        Integer expectedNumOfDice = 2;
+
+        Dice dice = new Dice(expectedNumOfDice);
+
+        Integer actualNumOfDice = dice.getNumOfDice();
+
+        Assert.assertEquals(expectedNumOfDice, actualNumOfDice);
+    }
+
+
+    @Test
+    //toss x # of dice y times and sum the results of the dice tosses
+    public void tossAndSumTest() {
+
+        Dice diceInstance = new Dice(1);
+        Integer toss = diceInstance.tossAndSum();
+        boolean actual;
+        boolean expected = true;
+
+        if (toss > 7 || toss < 1) {
+            actual = false;
+        } else {
+            actual = true;
+        }
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void tossAndSumTest2() {
+
+        Dice diceInstance = new Dice(2);
+        Integer toss = diceInstance.tossAndSum();
+        boolean actual;
+        boolean expected = true;
+
+        if (toss > 12 || toss < 2) {
+            actual = false;
+        } else {
+            actual = true;
+        }
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void tossAndSumTest3(){
+        Dice diceInstance = new Dice(3);
+        Integer toss = diceInstance.tossAndSum();
+        boolean actual;
+        boolean expected = true;
+
+        if(toss > 18 || toss < 3){
+            actual = false;
+        } else{
+            actual = true;
+        }
+
+        Assert.assertEquals(expected, actual);
+    }
+}
+
+
+
+
+
